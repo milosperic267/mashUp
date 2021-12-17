23df12d2fef13514cba8aa0a78259d8a2cc23f4d
milosperic267 <peric-milos@hotmail.com>
2021-12-17 15:15:51 +0100

    Initial commit

---
+++ src/main/java/ch/bbw/mashup/MashUpApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.mashup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MashUpApplication.class, args);
+    }
+
+}
